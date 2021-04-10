1069d8633bab9ac37d69afc19ac2c36d7c998cb3
José Miguel <MontevaniOthon@gmail.com>
2021-04-10 20:49:26 -0300

    home
    Melhorando o template da home

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
     #html = "<html><body>It is now %s.</body></html>" % now
 
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
