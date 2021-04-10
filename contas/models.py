89488bc657199cfb35cd175dbe2684e5823c15aa
José Miguel <MontevaniOthon@gmail.com>
2021-04-10 20:21:20 -0300

    Mais models
    Adicionando Transacao e melhorando Categoria

--- contas/models.py
+++ contas/models.py
@@ -4,3 +4,18 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=140) 
     dt_criacao = models.DateTimeField(auto_now_add=True) #Guarda a data automaticamente
     
+    def __str__(self):
+        return self.nome
+
+class Transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    #categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return self.descricao
